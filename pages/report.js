cd6d87131b9a9669f904cf9f13acab46227df71a
Juan <juansilupumaza@gmail.com>
2020-03-18 13:02:53 -0500

    new/list branch

--- pages/report.js
+++ pages/report.js
@@ -1,16 +1,4 @@
 import React from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-  ButtonToggle,
-  Row,
-  Col
-} from "reactstrap";
 import Header from "../components/Header";
 import BodyReport from "../components/BodyReport";
 
