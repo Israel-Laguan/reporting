e182895bb4ed83902ae7a7a048c715b2c8be9c2d
Israel-Laguan <israellaguan@gmail.com>
2020-03-23 16:13:34 -0500

    [fix] Edit user working
    What?:
    
    Breaking Changes?

--- components/FormUser.js
+++ components/FormUser.js
@@ -1,48 +1,57 @@
-import React from "react";
-import Link from "next/link";
-import Router from "next/router";
-import useForm from "../utilities/useForm";
-import withAuth from "../utils/withAuth";
-import swal from "sweetalert";
+import React from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+import useForm from '../utilities/useForm'
+import withAuth from '../utils/withAuth'
+import swal from 'sweetalert'
 
 const FormUser = ({ initialValues = {}, auth }) => {
-  const [isAdmin, setIsAdmin] = React.useState(false);
-  const [isBoss, setIsBoss] = React.useState(false);
-  const form = useForm({ initialValues });
+  const [isAdmin, setIsAdmin] = React.useState(false)
+  const [isBoss, setIsBoss] = React.useState(false)
+  const form = useForm({ initialValues })
 
   const handleSubmit = async e => {
-    e.preventDefault();
+    e.preventDefault()
     const headers = {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "x-access-token": auth.getToken()
-    };
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        ...form.fields
-      })
-    };
-    const res = await fetch("https://etl-auth.herokuapp.com/api/v1/user/", {
-      headers,
-      ...options
-    }).then(res => res.json());
-    const { success, errors, msg } = res;
-    if (!success) {
-      console.error(errors);
-      swal("Error!", msg, "error");
-    } else {
-      swal("Correcto!", msg, "success").then(() => {
-        Router.push("/users");
-      });
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-access-token': auth.getToken(),
     }
-  };
+    const options = {
+      method: initialValues ? 'PUT' : 'POST',
+      body: JSON.stringify({
+        ...form.fields,
+      }),
+    }
+    console.log(options)
+    const res = await fetch(
+      `http://localhost:8000/api/v1/user/${
+        initialValues ? initialValues._id : ''
+      }`,
+      {
+        headers,
+        ...options,
+      },
+    ).then(res => {
+      console.info(res)
+      return res.json()
+    })
+    const { success, errors, msg } = res
+    if (!success) {
+      console.error(errors)
+      swal('Error!', msg, 'error')
+    } else {
+      swal('Correcto!', msg, 'success').then(() => {
+        Router.push('/users')
+      })
+    }
+  }
 
   React.useEffect(() => {
     const userRole = localStorage.getItem('user_role')
-    if (userRole === 'ADMIN') setIsAdmin(true);
-    if (userRole === 'BOSS') setIsBoss(true);
-  },[]);
+    if (userRole === 'ADMIN') setIsAdmin(true)
+    if (userRole === 'BOSS') setIsBoss(true)
+  }, [])
 
   return (
     <form name="form" onSubmit={handleSubmit}>
@@ -51,7 +60,7 @@ const FormUser = ({ initialValues = {}, auth }) => {
         <input
           type="text"
           className="form-control"
-          {...form.getInput("name")}
+          {...form.getInput('name')}
           id="name"
           required
         />
@@ -61,41 +70,47 @@ const FormUser = ({ initialValues = {}, auth }) => {
         <input
           type="email"
           className="form-control"
-          {...form.getInput("email")}
+          {...form.getInput('email')}
           id="email"
           required
         />
       </div>
-      {
-        isBoss && <div className="form-group">
-        <label htmlFor="password">Contraseña</label>
-        <input
-          type="password"
-          className="form-control"
-          {...form.getInput("password")}
-          minLength="8"
-          required
-        />
-      </div>
-      }
+      {isAdmin && (
+        <div className="form-group">
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            {...form.getInput('password')}
+            minLength="8"
+          />
+        </div>
+      )}
       <div className="form-group">
         <label htmlFor="role">Rol</label>
-        <select className="form-control" {...form.getSelect("role")} required defaultValue="EMPLOYEE">
-          {isAdmin && <option value="ADMIN">Administrador</option> }
+        <select
+          className="form-control"
+          {...form.getSelect('role')}
+          required
+          defaultValue="EMPLOYEE"
+        >
+          {isAdmin && <option value="ADMIN">Administrador</option>}
           {isAdmin && <option value="BOSS">Jefe</option>}
           <option value="EMPLOYEE">Empleado</option>
         </select>
       </div>
 
       <div className="form-group d-flex justify-content-center">
-  <button className="btn btn-primary">{initialValues? 'Guardar' : 'Registrar'}</button>
-        {"  "}
+        <button className="btn btn-primary">
+          {initialValues ? 'Guardar' : 'Registrar'}
+        </button>
+        {'  '}
         <Link href="/report">
           <button className=" btn btn-secondary ml-3">Cancel</button>
         </Link>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default withAuth(FormUser);
+export default withAuth(FormUser)
